46d8fd8a7614f06b74ad7f012df51ef0d604d2db
axrorback <axrorback@gmail.com>
2026-03-10 02:45:32 +0500

    Implement blog views: list, detail, and create post functionality.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def post_list(request):
+    post = Post.objects.filter(is_active=True).order_by('-created_at')
+    context = {
+        'post': post
+    }
+    return render(request,'blog/post_list.html',context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post,slug=slug)
+    context = {
+        'post': post
+    }
+    return render(request,'blog/post_detail.html',context)
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_by = request.user
+            post.save()
+            messages.success(request, 'Post created successfully')
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request,'blog/post_create.html',context)
