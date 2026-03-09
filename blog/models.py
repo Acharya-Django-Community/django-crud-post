c7a681a938d76ed31d577ed7f2e99bb521e63126
axrorback <axrorback@gmail.com>
2026-03-10 02:45:27 +0500

    Enhance Post model: add slug generation, string representation, and associate with User model.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=25)
@@ -9,4 +11,13 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
-    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
