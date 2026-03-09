3b7aead70f30602062e671519a32411955ff7b98
axrorback <axrorback@gmail.com>
2026-03-10 02:12:37 +0500

    Adding Model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=25)
+    content = models.TextField()
+    slug = models.SlugField(unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    
